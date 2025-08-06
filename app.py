2fe7b7b9f6357415f1dcdfa8bed9eda6c07be4bd
Prithwijeet Rakshit <prithwijeet@gmail.com>
2025-08-06 11:34:24 +0530

    run updated

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import requests
 
@@ -35,4 +36,5 @@ def postback():
     return jsonify({"status": "received", "forwarded_to_local": True})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # default to 5000 for local dev
+    app.run(host='0.0.0.0', port=port)
