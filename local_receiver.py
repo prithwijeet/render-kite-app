f864129609ec71873439d9b382f1efeebdcb1ec8
Prithwijeet Rakshit <prithwijeet@gmail.com>
2025-08-06 12:54:10 +0530

    favicon added

--- local_receiver.py
+++ local_receiver.py
@@ -1,12 +1,14 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/handle_kite_event', methods=['POST'])
+@app.route('/handle_kite_event', methods=['GET', 'POST'])
 def handle_kite_event():
-    data = request.json
+    data = request.args.to_dict() if request.method == 'GET' else request.form.to_dict()
     print("Received from Render:", data)
     return jsonify({"status": "received by local app"})
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    port = int(os.environ.get('PORT', 5000))  # default to 5000 for local dev
+    app.run(host='0.0.0.0', port=port)
